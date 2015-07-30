603030ccdf4e2f1f0a17574a7a6c92f72de5ff48
Jonathan Peck <peckjonathan@gmail.com>
2015-07-30 20:37:35 +0200

    Fuck C++

---
+++ Hackers/decls.h
@@ -0,0 +1,29 @@
+//
+//  decls.h
+//  Hackers
+//
+//  Created by Jonathan Peck on 30/07/15.
+//  Copyright (c) 2015 Jonathan Peck. All rights reserved.
+//
+
+class Host;
+class Localhost;
+class Service;
+class SSHService;
+class FTPService;
+class ShellService;
+class SMTPService;
+class FinanceService;
+class IP;
+class Shell;
+class Internet;
+class Person;
+struct Resource;
+class ResourceGenerator;
+class Program;
+class ScannerProgram;
+class Exploit;
+class LogDeleter;
+class Robber;
+class Cracker;
+class TransDeleter;
